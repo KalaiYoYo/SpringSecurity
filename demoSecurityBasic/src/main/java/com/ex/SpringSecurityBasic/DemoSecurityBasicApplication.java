f2c3078cd0971139997dd18000bbc549f5328006
User <kalaichlvn36@gmail.com>
2022-04-28 09:20:14 +0530

    File Commit

---
+++ demoSecurityBasic/src/main/java/com/ex/SpringSecurityBasic/DemoSecurityBasicApplication.java
@@ -0,0 +1,13 @@
+package com.ex.SpringSecurityBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityBasicApplication.class, args);
+	}
+
+}

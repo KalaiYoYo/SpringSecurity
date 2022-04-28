86abf83a0bcc49618526f3f1d9d81e0e54f301a4
User <kalaichlvn36@gmail.com>
2022-04-28 09:26:16 +0530

    Commit1

---
+++ ReadProperty/src/main/java/com/ReadProperty/ReadPropertyApplication.java
@@ -0,0 +1,13 @@
+package com.ReadProperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadPropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadPropertyApplication.class, args);
+	}
+
+}
---
+++ ReadProperty/src/test/java/com/ReadProperty/ReadPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReadProperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadPropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
